4f2454b80f017cd596c45a7f28acec59c28878ce
asallh <ajaypalsallh@gmail.com>
2024-06-21 15:21:58 -0600

    utility issue resolved
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
