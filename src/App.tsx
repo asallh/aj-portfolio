4701e01eb29e2e65f96f0b2524ecac026fab3ed7
asallh <ajaypalsallh@gmail.com>
2024-06-22 10:14:04 -0600

    inial app saet
    Signed-off-by: asallh <ajaypalsallh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import VerticalTimelineComponent from "./Components/VerticalTimelineComponent";
+import { WavyBackground } from "./Components/ui/wavy-background";
+
 function App() {
   return (
-    <center>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
-    </center>
+    <>
+      <WavyBackground className="max-w-4xl mx-auto pb-40">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      </WavyBackground>
+      <VerticalTimelineComponent />
+    </>
   );
 }
 
